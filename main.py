45530043aa8fc8c661f561aef48c1a76c2d80999
Simona Cordos <cordos.simona@gmail.com>
2022-06-02 19:55:03 +0300

    add notebook

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from notebook import Notebook
 
-my_notebook = Notebook(50, "blue", 
-    ["Day one", "Day two",])
+my_notebook = Notebook(2, "blue")
 print(my_notebook.empty_pages())
 print(my_notebook.written_pages())
 
-my_notebook.write_to_notebook("Day three",)
+my_notebook.write_to_notebook("Day three")
+my_notebook.write_to_notebook("Day three")
+my_notebook.write_to_notebook("Day three")
 print(my_notebook.empty_pages())
 print(my_notebook.written_pages())
 
